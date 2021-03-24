b9a45b258e3019cf0114c9ce8bc80c1017166ef0
MauriPiace92 <75382063+MauriPiace92@users.noreply.github.com>
2021-03-24 09:52:05 +0100

    FINE

--- js/script.js
+++ js/script.js
@@ -2,30 +2,32 @@
 // al posto del numero e per i multipli di 5 stampi Buzz. 
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz. 
 
+// Devo Stampare  i 100 numeri:
 var miaLista =[];
 
 for (var i=1; i <= 100; i++){
 
-    if (i % 3 == 0){
+    if (i % 15 == 0){ //se divisibile per 3 e anche per 5 stampa "FizzBuzz"
     
-        console.log("Fizz");
-        miaLista.push("Fizz");
+        console.log("FizzBuzz");
+        miaLista.push("FizzBuzz");
     
-    }else if (i % 5 == 0){
+    }else if (i % 5 == 0){//Se divisibile per 5 stampa "buzz"
 
         console.log("Buzz");
         miaLista.push("Buzz");
     
-    }else if (i % 15 == 0){
+    }else if ( i % 3 == 0){//se divisibile per 3 stampa "Fizz"
         
-        console.log("FizzBuzz");
-        miaLista.push("FizzBuzz");
-    }else{
+        console.log("Fizz");
+        miaLista.push("Fizz");
+    }else{ // altrimenti stampa il numero normale
         
         console.log(i);
         miaLista.push(i);
     }     
 }
 
+//stampo i numeri:
 console.log(miaLista);
 alert("ECCO LA LISTA FIZZBUZZ: " + miaLista);
